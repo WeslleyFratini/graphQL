aadb605930eaa9f6e153f15c665334ea1d142398
WeslleyFratini <weeslleey354@gmail.com>
2021-05-05 19:51:37 -0300

    aula 03

---
+++ aula03/index.js
@@ -0,0 +1,22 @@
+const { gql, ApolloServer} = require('apollo-server')
+
+const resolvers = {
+  Query: {
+    hello(){
+      return 'World'
+    }
+  }
+}
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.listen()
