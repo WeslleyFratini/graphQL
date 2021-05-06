9efae9344590092877505f19adb8f8cb6b5791f8
WeslleyFratini <weeslleey354@gmail.com>
2021-05-06 19:33:53 -0300

    aula 07

---
+++ aula07/index.js
@@ -0,0 +1,77 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const produtos = [
+  {
+    id: 1,
+    nome: 'Computador',
+    valor: 3000.00
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    valor: 3000.00
+  }
+]
+
+const usuarios = [
+  {
+    id: 1,
+    nome: 'Paulo',
+    salario: 1234.54,
+    ativo: true,
+    idade: 23
+  },
+  {
+    id: 2,
+    nome: 'Lucas',
+    salario: 4321.54,
+    ativo: false,
+    idade: 30
+  }
+]
+
+const typeDefs = gql`
+  type Produto {
+    id: ID
+    nome: String
+    valor: Float
+  }
+
+  type Usuario {
+    idade: Int
+    salario: Float
+    nome: String
+    ativo: Boolean
+    id: ID
+  }
+
+  type Query{
+    usuarios: [Usuario]
+    produtos: [Produto]
+    usuario(id: Int, nome: String):Usuario
+  }
+
+`;
+
+const resolvers = {
+  Query: {
+    usuarios() {
+      return usuarios
+    },
+    usuario(_,args){
+      const { id, nome } = args
+      if(id) return usuarios.find((usuario)=> usuario.id == id)
+      return usuarios.find((usuario)=> usuario.nome === nome)
+    },
+    produtos() {
+      return produtos
+    }
+  }
+}
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  server.listen();
