314e575f3e4d468911be317b13195d6724410e2c
WeslleyFratini <weeslleey354@gmail.com>
2021-05-19 19:57:36 -0300

    aula 16

---
+++ aula16/src/graphql/modules/Usuario/resolvers.js
@@ -0,0 +1,74 @@
+const db = require("../../../db");
+
+function geradorDeId(lista) {
+  let novoId;
+  let ultimo = lista[lista.length - 1];
+  if (!ultimo) {
+    novoId = 0;
+  } else {
+    novoId = ultimo.id;
+  }
+
+  return ++novoId;
+}
+
+module.exports = {
+  Usuario: {
+    perfil(usuario) {
+      return db.perfis.find((p) => p.id === usuario.perfil_id);
+    },
+  },
+  Query: {
+    usuario(_, { data }) {
+      return db.usuarios.find((db) => db.id === data.id);
+    },
+    usuarios: () => db.usuarios,
+  },
+  Mutation: {
+    criarUsuario(_, { data }) {
+      const { email } = data;
+
+      const usuarioExistente = db.usuarios.some((u) => u.email === email);
+
+      if (usuarioExistente) {
+        throw new Error(`Usuário existente: ${data.nome}`);
+      }
+
+      const novoUsuario = {
+        ...data,
+        id: geradorDeId(db.usuarios),
+        perfil_id: 2,
+      };
+
+      db.usuarios.push(novoUsuario);
+
+      return novoUsuario;
+    },
+    atualizarUsuario(_, { id, data }) {
+      const usuario = db.usuarios.find((u) => u.id === id);
+      const indice = db.usuarios.findIndex((u) => u.id === id);
+
+      const novoUsuario = {
+        ...usuario,
+        ...data,
+      };
+
+      db.usuarios.splice(indice, 1, novoUsuario);
+
+      return novoUsuario;
+    },
+    deletarUsuario(_, { filtro: { id, email } }) {
+      return deletarUsuarioFiltro(id ? { id } : { email });
+    },
+  },
+};
+
+function deletarUsuarioFiltro(filtro) {
+  const chave = Object.keys(filtro)[0];
+  const valor = Object.values(filtro)[0];
+
+  const usuarioEncontrado = db.usuarios.find((u) => u[chave] === valor);
+  db.usuarios = db.usuarios.filter((u) => u[chave] !== valor);
+
+  return !!usuarioEncontrado;
+}
